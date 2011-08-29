06d14df961fba59ef32e501a530141f7973ab129
Stanislau <stanislau.wolski@gmail.com>
2011-08-29 11:04:05 +0300

    [add] initial import from svn

---
+++ WEB-INF/src/PDFGenerator.java
@@ -0,0 +1,20 @@
+
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import javax.servlet.http.*;
+
+import com.dhtmlx.scheduler.PDFWriter;
+
+
+
+@SuppressWarnings("serial")
+public class PDFGenerator extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+	throws IOException {
+		String xml;
+		xml = req.getParameter("mycoolxmlbody");
+		xml = URLDecoder.decode(xml, "UTF-8");
+		(new PDFWriter()).generate(xml, resp);
+	}
+}
