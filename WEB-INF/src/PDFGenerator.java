5681b3e6b308fd8bf264f1b7a290b522ab8ef519
dmitry-radyno <dmitry.radyno@gmail.com>
2012-07-25 15:02:42 +0200

    add debug mode

--- WEB-INF/src/PDFGenerator.java
+++ WEB-INF/src/PDFGenerator.java
@@ -1,3 +1,5 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URLDecoder;
 import javax.servlet.http.*;
@@ -8,11 +10,22 @@ import com.dhtmlx.scheduler.PDFWriter;
 
 @SuppressWarnings("serial")
 public class PDFGenerator extends HttpServlet {
+
+	protected Boolean debug = false;
+
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 	throws IOException {
 		String xml;
 		xml = req.getParameter("mycoolxmlbody");
 		xml = URLDecoder.decode(xml, "UTF-8");
+
+		if (this.debug) {
+			FileWriter fstream = new FileWriter("error_log.xml");
+			BufferedWriter out = new BufferedWriter(fstream);
+			out.write(xml);
+			out.close();
+		}
+
 		PDFWriter pdf = new PDFWriter();
 		pdf.generate(xml, resp);
 	}
